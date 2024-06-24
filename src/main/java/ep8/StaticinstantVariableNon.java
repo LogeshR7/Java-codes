3b2e27d9fa4e71d9fb806db0a1205d166f646dbe
LogeshR7 <Logeshwaran242001@gmail.com>
2024-06-24 14:57:14 +0530

    SEVENTH

---
+++ src/main/java/ep8/StaticinstantVariableNon.java
@@ -0,0 +1,37 @@
+package ep8;
+
+public class StaticinstantVariableNon {
+	
+	int x;
+	static int y;
+	
+	public void  incrment() {
+		x++;
+		y++;
+		System.out.println("Non-static-----"+x+"   Static----"+y);
+	}
+	
+	
+
+
+
+public static void main(String[] args) {
+
+	StaticinstantVariableNon st = new StaticinstantVariableNon();
+	st.incrment();
+	st.incrment();
+	
+	
+	
+	System.out.println("   gap   ");
+	
+	 StaticinstantVariableNon st1 = new StaticinstantVariableNon();
+	 st1.incrment();
+	 st1.incrment();
+	 
+}
+
+   
+   
+
+}
