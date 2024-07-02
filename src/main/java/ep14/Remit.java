bf349b5fb7789fdf1771a5cbd15a283a582239e9
LogeshR7 <Logeshwaran242001@gmail.com>
2024-07-02 17:23:03 +0530

    SE 1.4

--- src/main/java/ep14/Remit.java
+++ src/main/java/ep14/Remit.java
@@ -1,6 +1,7 @@
 package ep14;
 
 import org.openqa.selenium.By;
+import org.openqa.selenium.Keys;
 import org.openqa.selenium.WebDriver;
 import org.openqa.selenium.chrome.ChromeDriver;
 
@@ -14,7 +15,8 @@ public class Remit {
 	driver.manage().window().maximize();
 	
 	//Login
-	driver.findElement(By.id("login")).sendKeys("dosth");
+	driver.findElement(By.id("login")).sendKeys("dosth" , Keys.TAB);
+	Thread.sleep(3000);
 	driver.findElement(By.id("password")).sendKeys("itsmedosth");
 	driver.findElement(By.xpath("//button[text()='Login']")).click();
 	
@@ -23,6 +25,7 @@ public class Remit {
 	driver.findElement(By.xpath("(//a[text()='List'])[2]")).click();
 	Thread.sleep(3000);
 	//driver.findElement(By.xpath("//a[text()='R000006']")).click(); This redirect to next page need to study
+	//Jornal
 	driver.findElement(By.xpath("(//a[@class='nav-item-hold'])[3]")).click();
 	driver.findElement(By.xpath("(//a[text()='Listing'])[3]")).click();
 	driver.findElement(By.xpath("(//span[@class='checkmark'])[1]")).click();
@@ -30,7 +33,26 @@ public class Remit {
 	driver.findElement(By.id("Search_ledger")).click();
 	Thread.sleep(2000);
 	driver.findElement(By.xpath("(//i[@title='Edit'])[1]")).click();
-	driver.findElement(By.xpath("//input[@placeholder='Remarks From']")).sendKeys("loganathan");
+	//driver.findElement(By.xpath("//input[@placeholder='Remarks From']")).sendKeys("loganathan"); POP up need to study
+	
+	//Balance
+	driver.findElement(By.xpath("(//span[text()='Balance'])[1]")).click();
+	String value = driver.findElement(By.xpath("//select[@name='location']")).getAttribute("value");
+	System.out.println(value);
+	Thread.sleep(2000);
+	//driver.findElement(By.xpath("(//a[@target='_blank'])[1]")).click();
+	
+	//Day close
+	driver.findElement(By.xpath("(//a[@class='nav-item-hold'])[5]")).click();
+	String isEditable = driver.findElement(By.name("closed_by")).getAttribute("readonly");
+	System.out.println(isEditable);
+	boolean enabled = driver.findElement(By.name("closed_date")).isEnabled();
+	System.out.println(enabled);
+	
+	//Logout
+	driver.findElement(By.xpath("(//a[@class='nav-item-hold'])[6]")).click();
+	
+	driver.quit();
 	
 	}
 
---
+++ src/main/java/ep15/HandlingInput.java
@@ -0,0 +1,51 @@
+package ep15;
+
+import org.openqa.selenium.By;
+import org.openqa.selenium.Keys;
+import org.openqa.selenium.WebDriver;
+import org.openqa.selenium.chrome.ChromeDriver;
+
+import io.github.bonigarcia.wdm.WebDriverManager;
+
+public class HandlingInput {
+	public static void main(String[] args) {
+		WebDriverManager.chromedriver().setup();
+		WebDriver driver = new ChromeDriver();
+		driver.get("https://letcode.in/edit");
+		driver.manage().window().maximize();
+		
+		
+		//Enter you full name
+		driver.findElement(By.xpath("//input[@placeholder='Enter first & last name']")).sendKeys("Logeshwaran");
+		
+		//Append a text and press keyboard tab
+		driver.findElement(By.id("join")).sendKeys(" dude" , Keys.TAB);
+		
+		//What is inside the text box
+		String getMe = driver.findElement(By.id("getMe")).getAttribute("value");
+		System.out.println(getMe);
+		
+		//Clear the text
+		driver.findElement(By.id("clearMe")).clear();
+		
+		//Confirm edit field is disabled
+		boolean isenabled =  driver.findElement(By.xpath("//input[@id='noEdit']")).isEnabled();
+		System.out.println(isenabled);
+		
+		//Confirm  text is readonly
+		String isReadonly = driver.findElement(By.id("dontwrite")).getAttribute("readonly");
+		System.out.println(isReadonly);
+		//Quit
+		driver.quit();
+		
+		
+		
+		
+		
+	
+	
+	
+	
+	}
+
+}
